123dc33ba8bd78414695a43598a15045273b2740
kalhararajapaksha <kasunkrajapaksha@gmail.com>
2021-11-07 04:38:28 +0530

    connect with databse
    add con string

--- app/src/main/java/com/example/datasendv2/MainActivity.java
+++ app/src/main/java/com/example/datasendv2/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.datasendv2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText phno,bot,bat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        phno = (EditText) findViewById( R.id.phoneno);
+        bot = (EditText) findViewById(R.id.bottles);
+        bat = (EditText) findViewById(R.id.battery);
+    }
+
+    public void loginBtn(View view) {
+        String phoneN = phno.getText().toString();
+        String bottel = bot.getText().toString();
+        String battery = bat.getText().toString();
+
+        background bg = new background(this);
+        bg.execute(phoneN,bottel,battery);
     }
 }
